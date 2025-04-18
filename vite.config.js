0448386275f1b72fd01412aad30c4198ccdea822
DataByIO <ro_bot__@naver.com>
2025-04-18 16:51:44 +0900

    Init

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 5173, // 프론트 포트
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // 백엔드 서버 주소
+        ws: false,
+        PathRewrite: {
+          '^/': ''
+        }
+      }
+    }
+  }
 })
