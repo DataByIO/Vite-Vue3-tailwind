a75231ee909c94bf0c16ac7dd664d3d7b3c47a84
DataByIO <ro_bot__@naver.com>
2025-04-25 10:33:55 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+
 
 <template>
   <div class="bg-gray-900 text-white p-10 text-center">
     <h1 class="text-4xl font-bold">Tailwind 작동 확인</h1>
   </div>
+<!--  <Header/>-->
+<!--  <RouterView/>-->
+<!--  <Footer/>-->
 </template>
+
+<script>
+import VueHeader from "@/components/VueHeader.vue";
+import VueFooter from "@/components/VueFooter.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Header: VueHeader,
+    Footer: VueFooter,
+  }
+}
+</script>
 
 <style scoped>
 .logo {
